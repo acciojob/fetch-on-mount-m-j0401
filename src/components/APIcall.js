4f6656db6470ef9cc3730ae748b5a53ce7cc360a
Mehul Jain <mehul.jain2206@gmail.com>
2026-04-09 12:36:27 +0530

    fetch

---
+++ src/components/APIcall.js
@@ -0,0 +1,33 @@
+
+import React, {useEffect,useState} from "react";
+import axios from "axios";
+
+const APIcall=()=>{
+    let [div,setDiv]=useState([])
+   
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(res=> setDiv(res.data))
+        .catch(err=> err);
+      },[])
+
+    return (
+    <div>
+             <ol>
+                {
+            div.map((data,ind)=>
+              <div key={ind}>
+             <li style={{fontSize:"20px"}}>  <h1>{data.title}</h1> </li>
+            <p>{data.body}</p>
+            </div>
+
+)
+}
+</ol>
+        
+
+
+        </div>
+    )
+}
+export default APIcall;
